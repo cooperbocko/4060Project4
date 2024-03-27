e86a036e819ad8bc88e4373eb767b0ee1e274487
KrishnaM26 <krishna.mehta26@yahoo.com>
2024-03-27 17:50:32 -0400

    Creating views
    created splash, results, and view page.

---
+++ app/src/main/java/edu/uga/cs/mobileproject4/QuizPage.java
@@ -0,0 +1,14 @@
+package edu.uga.cs.mobileproject4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_page);
+    }
+}
