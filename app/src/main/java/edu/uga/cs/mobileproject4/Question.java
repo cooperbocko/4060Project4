d8f4af2670763b0c2686ec38f36776c1e31c4f91
cooper-bocko-uga <105941591+cooperbocko@users.noreply.github.com>
2024-04-05 21:43:14 -0400

    Passing Data to Fragments
    now able to pass data to fragments

---
+++ app/src/main/java/edu/uga/cs/mobileproject4/Question.java
@@ -0,0 +1,72 @@
+package edu.uga.cs.mobileproject4;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Question {
+    private static final String DEBUG = "Question";
+    public static List<String> continents = Arrays.asList("Asia", "Africa" , "North America", "South America", "Antarctica", "Europe", "Australia");
+    public static List<CountryModel> countryModels;
+    public static List<List<String>> questions;
+
+    //contructor
+    Question(List<CountryModel> x){
+        countryModels = x;
+    }
+    //makes the questions
+    public static void makeQuestions() {
+        //arrrays
+        questions = new ArrayList<>();
+
+        //make sure countryModels are supplied
+        if (countryModels == null) {
+            Log.d(DEBUG, "countryModels null");
+            return;
+        }
+
+        List<CountryModel> randomcountries = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            int rand = (int) (Math.random() * countryModels.size());
+            if (!randomcountries.contains(countryModels.get(rand))) {
+                randomcountries.add(countryModels.get(rand));
+            } else {
+                i--;
+                continue;
+            }
+        }
+        Log.d(DEBUG, "Random Countries: " + randomcountries.toString());
+
+        //make six questions
+        for (int i = 0; i < 6; i++) {
+            List<String> question = new ArrayList<>();
+            //insert name first
+            question.add(randomcountries.get(i).getName());
+            //insert the continents randomly
+            List<String> randomcontinents = new ArrayList<>();
+            randomcontinents.add(randomcountries.get(i).getContinent());
+            for (int j = 0; j < 3; j++) {
+                int rand = (int) (Math.random() * continents.size());
+                if (!randomcontinents.contains(continents.get(rand))){
+                    randomcontinents.add(continents.get(rand));
+                } else {
+                    j--;
+                    continue;
+                }
+            }
+            Log.d(DEBUG, "random continents: " + randomcontinents.toString());
+            for (int j = 0; j < 4; j++) {
+                int rand = (int) (Math.random() * randomcontinents.size());
+                question.add(randomcontinents.get(rand));
+                randomcontinents.remove(rand);
+            }
+            Log.d(DEBUG, "question: " + question.toString());
+            questions.add(question);
+        }
+        Log.d(DEBUG, "questions: " + questions.toString());
+    }
+
+
+}
--- app/src/main/java/edu/uga/cs/mobileproject4/ResultsFragment.java
+++ app/src/main/java/edu/uga/cs/mobileproject4/ResultsFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import java.util.List;
 /**
@@ -25,13 +26,12 @@ public class ResultsFragment extends Fragment {
 
     private ListView resultsList;
 
-
-
     //Getting quiz results from DB
     private class QuizDB extends AsyncTask<Void, List<QuizModel>>{
 
         @Override
         protected List<QuizModel> doInBackground(Void... arguments) {
+            //getting quiz models
             List<QuizModel> quizList = data.getQuizzes();
             Log.d(DEBUG, "QuizDB: Quizzes: " + quizList.size());
 
@@ -39,8 +39,11 @@ public class ResultsFragment extends Fragment {
         }
 
         @Override
-        protected void onPostExecute(List<QuizModel> quizModels) {
-
+        protected void onPostExecute(List<QuizModel> qModels) {
+            //adding quiz models to the listview
+            ArrayAdapter<QuizModel> quizAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, qModels);
+            resultsList.setAdapter(quizAdapter);
+            Log.d(DEBUG, "OnPostExecute: setting the quiz results to the adapter");
         }
     }
 
@@ -71,10 +74,37 @@ public class ResultsFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView =  inflater.inflate(R.layout.fragment_results, container, false);
 
+        //views
         resultsList = (ListView) rootView.findViewById(R.id.listView);
 
+        //getting quiz results and adding them to the listview
+        data = new Data(getActivity());
+        data.open();
+        new QuizDB().execute();
 
-
+        //
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //open db
+        if (data != null && !data.isDBOpen()) {
+            data.open();
+            Log.d(DEBUG, "Opening db");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        //close db
+        if (data != null) {
+            data.close();
+            Log.d(DEBUG, "Closing db");
+        }
     }
 }
